f92471c7cecbc639f5ceed94f68b72d9a39c67ef
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-26 00:05:28 +0200

    btn padding

--- apps/client/src/components/buttons/AuthBtn.tsx
+++ apps/client/src/components/buttons/AuthBtn.tsx
@@ -2,7 +2,7 @@ const AuthBtn = () => {
   return (
     <button
       type="submit"
-      className="py-4 px-5 bg-black w-full rounded-full text-white font-medium text-sm hover:bg-opacity-90 transition-colors duration-200"
+      className="py-5 px-5 bg-black w-full rounded-full text-white font-medium text-sm hover:bg-opacity-90 transition-colors duration-200"
     >
       Sign in
     </button>
