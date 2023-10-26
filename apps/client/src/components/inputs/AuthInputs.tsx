9bd5cfe8643fba18da5366e71e5bcaaabbbe75a8
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-26 11:38:09 +0200

    sign in

--- apps/client/src/components/inputs/AuthInputs.tsx
+++ apps/client/src/components/inputs/AuthInputs.tsx
@@ -1,12 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const AuthInputs = () => {
   return (
     <div className="w-full">
-      <label htmlFor="" className="font-medium text-sm">
-        Username or Email
-      </label>
+      <div>
+        <label htmlFor="" className="font-medium text-[15px]">
+          Username or Email
+        </label>
+        <Link
+          to="/forgot-password"
+          className=" underline font-normal float-right text-[15px]"
+        >
+          Forgot ?
+        </Link>
+      </div>
       <input
         type="text"
-        className="mt-1 w-full border-2 border-neutral-200 px-5 py-3.5 rounded-xl font-roboto font-light outline-blue-400 hover:input-shadow focus:input-shadow"
+        className="mt-1 w-full border-2 border-neutral-200 px-5 py-3.5 rounded-xl font-roboto text-[15px] font-light outline-blue-400 hover:input-shadow focus:input-shadow"
       />
     </div>
   );
--- apps/client/src/components/sections/auth/sign-in/SignIn.tsx
+++ apps/client/src/components/sections/auth/sign-in/SignIn.tsx
@@ -8,9 +8,7 @@ const SignIn = () => {
   return (
     <div className="fixed backdrop flex items-center justify-center top-0 left-0 bottom-0 right-0 w-screen h-screen">
       <div className="relative auth-container">
-        <h1 className="text-center mb-10 text-[24px] font-medium">
-          {signIn.label}
-        </h1>
+        <h1 className=" mb-10 text-[22px] font-semibold">{signIn.label}</h1>
         <form action="" className="w-full flex flex-col items-center gap-8">
           <AuthInputs />
           <AuthInputs />
@@ -20,7 +18,7 @@ const SignIn = () => {
         <div className="flex justify-center mt-6">
           <p className="mx-auto flex gap-2 font-light text-sm">
             <span>{signIn.noAccountLabel}</span>
-            <Link to="/sign-up" className="text-blue-500 underline font-normal">
+            <Link to="/sign-up" className=" underline font-normal">
               Sign Up
             </Link>
           </p>
